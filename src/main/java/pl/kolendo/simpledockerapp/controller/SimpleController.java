2245b8523c93fb0a8f86eee2da2a32ac91c789b1
AlionaVi <alionakolendo@gmail.com>
2023-08-01 16:16:37 +0300

    Simple docker application

---
+++ src/main/java/pl/kolendo/simpledockerapp/controller/SimpleController.java
@@ -0,0 +1,13 @@
+package pl.kolendo.simpledockerapp.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SimpleController {
+
+    @GetMapping("/")
+    public String hello () {
+        return "Hello Docker!";
+    }
+}
